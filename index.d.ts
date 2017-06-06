3530fd9e73d2e3195653d12ffa3869fb4db3be18
Omar Alshaker <omar@omaralshaker.com>
2017-06-06 14:07:44 +0200

    Add Typescript typings

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Determines whether a given value is an array.
+ * Returns a boolean with the result.
+ *
+ * @param arr The array you want to test
+ * @return {Boolean} 
+ */
+export default function isArray(arr: any): arr is any[];
