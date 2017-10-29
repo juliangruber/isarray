6fe802feb9386289c62f13f1aef64bd5d5d866ad
Haroen Viaene <fingebimus@me.com>
2017-10-29 08:38:42 +0100

    Test the package with explicitly removed Array.isArray
    try at making #3 mergeable

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 var isArray = require('./');
 var test = require('tape');
 
-test('is array', function(t){
+var nativeIsArray = Array.isArray;
+
+test('is array (only polyfill)', function(t){
+  delete Array.isArray;
   t.ok(isArray([]));
   t.notOk(isArray({}));
   t.notOk(isArray(null));
@@ -19,3 +22,21 @@ test('is array', function(t){
   t.end();
 });
 
+test('is array (native)', function(t){
+  Array.isArray = nativeIsArray;
+  t.ok(isArray([]));
+  t.notOk(isArray({}));
+  t.notOk(isArray(null));
+  t.notOk(isArray(false));
+  t.notOk(isArray(""));
+
+  var obj = {};
+  obj[0] = true;
+  t.notOk(isArray(obj));
+
+  var arr = [];
+  arr.foo = 'bar';
+  t.ok(isArray(arr));
+
+  t.end();
+});
