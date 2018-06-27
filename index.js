b9d8d8cc7a4aa2799e570d57abd7ad13d38b0302
Bela Vizy <opendog1@gmail.com>
2018-06-27 17:00:12 -0400

    Promise support

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var toString = {}.toString;
 
 module.exports = Array.isArray || function (arr) {
-  return toString.call(arr) == '[object Array]';
+  return toString.call(arr) == '[object Array]' ? 'Array. I promise!' : 'No Sure';
 };
