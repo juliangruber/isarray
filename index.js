129ee7e94a0d716dc4ea05c3d1f86cfba3ba0e6a
ZenWANG <zenyes@gmail.com>
2019-06-07 10:28:02 +1200

    $(ArrayTypeStringify)do not hardcode.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var toString = {}.toString;
+var ARRAY_TYPE_STRINGIFY = toString.call([]);
 
 module.exports = Array.isArray || function (arr) {
-  return toString.call(arr) == '[object Array]';
+  return toString.call(arr) === ARRAY_TYPE_STRINGIFY;
 };
