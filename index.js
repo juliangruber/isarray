96309081fae20d06c020875fa8f098fe8af722a6
GottZ <git@gottz.de>
2021-06-08 04:32:32 +0200

    use instanceof instead of string casting

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var toString = {}.toString;
-
-module.exports = Array.isArray || function (arr) {
-  return toString.call(arr) === '[object Array]';
+module.exports = Array.isArray || function isArray (arr) {
+  return arr instanceof Array;
 };
