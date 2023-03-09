20005ea24ade3cd345f25f84aa80a5814ec8b9a2
BlobTheKat <64211737+BlobTheKat@users.noreply.github.com>
2023-03-09 23:16:02 +0000

    More robust implementation

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-'use strict';
+// Node has been supporting this from day 1
 
-var toString = {}.toString;
+// Browsers have been supporting Array.isArray
+// for longer than even strict mode
 
-module.exports = Array.isArray || function (arr) {
-  return toString.call(arr) === '[object Array]';
-};
+// Additionally, every single even remotely
+// popular tool capable of bringing node
+// modules to the web do not even target
+// browser versions older than Array.isArray
+// In fact, most of them use `Array.isArray`
+
+// Finally, Array.isArray and JSON.stringify
+// are the only ways to reliably check
+// if an object is internally an array
+// The previous solution checked if the given
+// object has a string tag of "Array", which
+// may be true for non-array objects
+
+module.exports = Array.isArray
