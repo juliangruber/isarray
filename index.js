e8fe4c772e18ea72a15a2dd98e03993aaca91e78
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2023-04-23 19:45:14 +0500

    Rework the array check

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var toString = {}.toString;
-
 module.exports = Array.isArray || function (arr) {
-  return toString.call(arr) === '[object Array]';
+  return arr && arr.constructor === Array;
 };
