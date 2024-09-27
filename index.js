1762f3301c57e649fc7828840addd747c69c1378
rando <158522817+tj-commits@users.noreply.github.com>
2024-09-27 17:22:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-var arrayObjectStringTag = '[object Array]';
 var toString = Object.prototype.toString;
 
 module.exports = Array.isArray || function (arr) {
-  return toString.call(arr) === arrayObjectStringTag;
+  return toString.call(arr) === '[object Array]';
 };
